7f2d1c1b6fb50d1f286f50249237507c7c99d6ca
Kaushik Devireddy <kaushik.devireddy@warriorlife.net>
2022-02-18 19:28:13 -0800

    python black formatting

--- dassana/rest.py
+++ dassana/rest.py
@@ -6,20 +6,27 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from urllib3.exceptions import MaxRetryError
 
-def forward_logs(log_data, endpoint=get_endpoint(), token=get_token(), app_id=get_app_id(), use_ssl=get_ssl()):
+
+def forward_logs(
+    log_data,
+    endpoint=get_endpoint(),
+    token=get_token(),
+    app_id=get_app_id(),
+    use_ssl=get_ssl(),
+):
 
     headers = {
-      'x-dassana-token': token,
-      'x-dassana-app-id': app_id,
-      'Content-type': 'application/x-ndjson',
-      'Content-encoding': 'gzip'
+        "x-dassana-token": token,
+        "x-dassana-app-id": app_id,
+        "Content-type": "application/x-ndjson",
+        "Content-encoding": "gzip",
     }
 
     retry = Retry(
         total=3,
         status_forcelist=[429, 500, 502, 503, 504],
         backoff_factor=1,
-        method_whitelist=["POST"]
+        method_whitelist=["POST"],
     )
 
     http = requests.Session()
@@ -27,8 +34,10 @@ def forward_logs(log_data, endpoint=get_endpoint(), token=get_token(), app_id=ge
     http.mount("http://", adapter)
     http.mount("https://", adapter)
 
-    payload = '\n'.join(dumps(log) for log in log_data) + '\n'
-    payload_compressed = gzip.compress(payload.encode('utf-8'))
+    payload = "\n".join(dumps(log) for log in log_data) + "\n"
+    payload_compressed = gzip.compress(payload.encode("utf-8"))
 
-    response = http.post(endpoint, headers=headers, data=payload_compressed, verify=use_ssl)
+    response = http.post(
+        endpoint, headers=headers, data=payload_compressed, verify=use_ssl
+    )
     return response
