9d451e200f53436e22dcfee4df4d72207d268191
neeraj-dassana <111750683+neeraj-dassana@users.noreply.github.com>
2023-01-19 22:21:19 +0530

    version update (#30)
    * ssl fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dassana",
-    version="0.7.17",
+    version="0.7.18",
     description="Dassana common data ingestion utilities",
     long_description="Dassana common data ingestion utilities",
     url="https://github.com/dassana-io/dassana-python",
