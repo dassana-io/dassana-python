2256823f45a4ed5e992fd0262bc9f3116bc5c4fc
akashdeep-dassana <111749091+akashdeep-dassana@users.noreply.github.com>
2023-07-13 00:11:37 +0530

    Json lib change (#61)
    * Changed json to ujson
    
    Changed json library to ujson for faster processing and to resolve decimal errors from few apps.
    
    * Update version and add ujson support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dassana",
-    version="0.8.13",
+    version="0.8.14",
     description="Dassana common data ingestion utilities",
     long_description="Dassana common data ingestion utilities",
     url="https://github.com/dassana-io/dassana-python",
@@ -10,6 +10,6 @@ setup(
     author_email="support@dassana.io",
     license="MIT",
     packages=["dassana"],
-    install_requires=["certifi", "requests", "urllib3", "google-cloud-pubsub", "google-cloud-storage", "boto3"],
+    install_requires=["certifi", "requests", "urllib3", "google-cloud-pubsub", "google-cloud-storage", "boto3", "ujson"],
     zip_safe=False,
 )
