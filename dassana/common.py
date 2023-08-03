845c8bec55510bc1aa1f4af4427c9417170c21e9
akashdeep-dassana <111749091+akashdeep-dassana@users.noreply.github.com>
2023-08-04 00:26:12 +0530

    Exception handling (#64)
    * Exception handling
    
    * Update key names for metadata
    
    * Change value type
    
    Change pass and fail values from str to int
    
    * Added retries for all requests

--- dassana/common.py
+++ dassana/common.py
@@ -8,6 +8,7 @@ import requests
 import logging
 from .dassana_env import *
 import datetime
+from tenacity import retry, wait_fixed, stop_after_attempt
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,13 +22,23 @@ client_secret = get_client_secret()
 class AuthenticationError(Exception):
     """Exception Raised when credentials in configuration are invalid"""
 
+    def __init__(self, message, response):
+        super().__init__()
+        self.message = message
+        self.response = response
+
+    def __str__(self):
+        return f"{self}"
+
+class ExternalError(Exception):
+    """Exception Raised when credentials in configuration are invalid"""
+
     def __init__(self, message):
         super().__init__()
         self.message = message
 
     def __str__(self):
-        return f"Source authentication failure: {self.message}"
-
+        return f"{self}"
 
 class InternalError(Exception):
     """Exception Raised for AppServices, Ingestion, or Upstream
@@ -42,7 +53,7 @@ class InternalError(Exception):
         super().__init__(self.message)
 
     def __str__(self):
-        return json.dumps({"source": self.source, "message": self.message})
+        return f"{self}"
 
 class StageWriteFailure(Exception):
     """Exception for StageWriteFailure"""
@@ -51,13 +62,14 @@ class StageWriteFailure(Exception):
         self.message = message
 
     def __str__(self):
-        return f"Stage Write Failure: {self.message}"
+        return f"{self}"
 
 def datetime_handler(val):
     if isinstance(val, datetime.datetime):
         return val.isoformat()
     return str(val)
 
+@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def get_ingestion_config(ingestion_config_id, app_id, tenant_id):
     url = f"https://{app_url}/app/{app_id}/ingestionConfig/{ingestion_config_id}"
     access_token = get_access_token()
@@ -71,10 +83,11 @@ def get_ingestion_config(ingestion_config_id, app_id, tenant_id):
         response = requests.request("GET", url, headers=headers)
     try:
         ingestion_config = response.json() 
-    except:
-        raise InternalError(url, response.json())
+    except Exception as e:
+        raise InternalError("Failed to get ingestion config", "Error getting response from app-manager with response body: " + str(response.text) + " and response header: " + str(response.headers) + " and stack trace: " +  str(e))
     return ingestion_config
 
+@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def patch_ingestion_config(payload, ingestion_config_id, app_id, tenant_id):
     url = f"https://{app_url}/app/{app_id}/ingestionConfig/{ingestion_config_id}"
     access_token = get_access_token()
@@ -89,6 +102,7 @@ def patch_ingestion_config(payload, ingestion_config_id, app_id, tenant_id):
     
     return response.status_code
 
+@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def get_access_token():
     url = f"{auth_url}/oauth/token"
     if auth_url.endswith("svc.cluster.local"):
@@ -112,11 +126,13 @@ def get_access_token():
         )
     try:
         access_token = response.json()["access_token"]
-    except:
-        raise InternalError(url, response.json())
+    except Exception as e:
+        raise InternalError("Failed to get access token", "Error getting response from app-manager with response body: " + str(response.text) + " and response header: " + str(response.headers) + " and stack trace: " +  str(e))
+
     return access_token
 
-def update_ingestion_to_done(job_id, tenant_id, metadata = {}):
+@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
+def update_ingestion_to_done(job_id, tenant_id, metadata):
     
     access_token = get_access_token()
     headers = {
@@ -129,6 +145,7 @@ def update_ingestion_to_done(job_id, tenant_id, metadata = {}):
     print("Ingestion status updated to done")
     return res.json()
 
+@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def cancel_ingestion_job(job_id, tenant_id, metadata, fail_type):
     
     access_token = get_access_token()
@@ -142,6 +159,7 @@ def cancel_ingestion_job(job_id, tenant_id, metadata, fail_type):
     print("Ingestion status updated to failed")
     return res.json()
 
+@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def get_ingestion_details(tenant_id, source, record_type, config_id, metadata, priority, is_snapshot):
     access_token = get_access_token()
 
@@ -167,6 +185,7 @@ def get_ingestion_details(tenant_id, source, record_type, config_id, metadata, p
 
     return 0
 
+@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def report_status(status, additionalContext, timeTakenInSec, recordsIngested, ingestion_config_id, app_id, tenant_id):
     reportingURL = f"https://{app_url}/app/v1/{app_id}/status"
 
@@ -228,10 +247,11 @@ class DassanaWriter:
     def initialize_client(self, tenant_id, source, record_type, config_id,  metadata, priority, is_snapshot):
         try:
             response = get_ingestion_details(tenant_id, source, record_type, config_id, metadata, priority, is_snapshot)
+            
             service = response['stageDetails']['cloud']
             self.job_id = response["jobId"]
         except Exception as e:
-            raise InternalError("Failed to create ingestion job",e)
+            raise InternalError("Failed to create ingestion job", "Error getting response from ingestion-srv with response body: " + str(response.text) + " and response header: " + str(response.headers) + " and stack trace: " +  str(e))
 
 
         if service == 'gcp':
@@ -299,14 +319,61 @@ class DassanaWriter:
         if os.path.exists("service_account.json"):
             os.remove("service_account.json")
 
-    def close(self):
+    def cancel_job(self, exception_from_src):
+        if os.path.exists("service_account.json"):
+            os.remove("service_account.json")
+        try:
+            if(type(exception_from_src).__name__ == "ExternalError"):
+                metadata = {}
+                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": [str(exception_from_src)], "error_code": "other_error"}
+                metadata["job_result"] = job_result
+                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "failed")
+
+            elif(type(exception_from_src).__name__ == "AuthenticationError"):
+                
+                debug_log = ["Auth Response: " + str(exception_from_src.response) + " Stack Trace: " + str(exception_from_src)]
+                metadata = {}
+                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": debug_log, "error_code": "auth_error"}
+                metadata["job_result"] = job_result
+                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "failed")
+
+            elif(type(exception_from_src).__name__ == "InternalError"):
+                metadata = {}
+                job_result = {"failure_reason": exception_from_src.message, "status": "cancel", "debug_log": [str(exception_from_src)], "error_code": "other_error"}
+                metadata["job_result"] = job_result
+                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "cancel")
+
+            elif(type(exception_from_src).__name__ == "StageWriteFailure"):
+                metadata = {}
+                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": [str(exception_from_src)], "error_code": "stage_write_failure"}
+                metadata["job_result"] = job_result
+                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "failed")
+            
+            else:
+                metadata = {}
+                job_result = {"failure_reason": str(exception_from_src), "status": "cancel", "debug_log": [str(exception_from_src)], "error_code": "other_error"}
+                metadata["job_result"] = job_result
+                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "cancel")
+        
+        except Exception as e:
+            metadata = {}
+            job_result = {"failure_reason": str(e), "status": "cancel", "debug_log": [str(e)], "error_code": "other_error"}
+            try:
+                cancel_ingestion_job(self.job_id, self.tenant_id, {}, "cancel")
+            except:
+                raise
+            
+    def close(self, pass_counter, fail_counter):
         self.file.close()
+        metadata = {}
+        job_result = {"status": "done", "pass" : int(pass_counter), "fail": int(fail_counter)}
+        metadata["job_result"] = job_result
         if self.bytes_written > 0:
             self.compress_file()
             self.upload_to_cloud()
             print(f"Ingested remaining data: {self.bytes_written} bytes")
             self.bytes_written = 0
-        update_ingestion_to_done(self.job_id, self.tenant_id)
+        update_ingestion_to_done(self.job_id, self.tenant_id, metadata)
         if os.path.exists("service_account.json"):
             os.remove("service_account.json")
 
