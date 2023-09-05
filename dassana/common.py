f84ee592f4b7d172c9bdef8fa9140ab38b56884d
akashdeep-dassana <111749091+akashdeep-dassana@users.noreply.github.com>
2023-09-05 16:29:28 +0530

    Add token (#69)
    * Update env vars
    
    1. Add is_internal_auth() to check if it's internal token
    2. Add get_dassana_token() to get dassana token
    3. Added internal auth check for tenant id
    
    * Function restructure
    
    1. Add dassana token support
    2. Move all job related functions inside DassanaWriter class
    
    * Added dt_token support
    
    * Signed url support
    
    * Fix errors
    
    * Change signed url method
    
    1. Not adding signed url to not use class attribute
    2. Change status ready_for_download to ready_for_loading in metadata
    3. Change status cancel to canceled in metadata
    
    * Update status
    
    * Modify pass,fail and debug log
    
    Added pass,fail and debug log support in class DassanaWriter
    
    * Add loggers
    
    * Update version

--- dassana/common.py
+++ dassana/common.py
@@ -12,13 +12,6 @@ from tenacity import retry, wait_fixed, stop_after_attempt
 
 logging.basicConfig(level=logging.INFO)
 
-auth_url = get_auth_url()
-app_url = get_app_url()
-debug = get_if_debug()
-ingestion_service_url = get_ingestion_srv_url()
-client_id = get_client_id()
-client_secret = get_client_secret()
-
 class AuthenticationError(Exception):
     """Exception Raised when credentials in configuration are invalid"""
 
@@ -70,14 +63,29 @@ def datetime_handler(val):
         return val.isoformat()
     return str(val)
 
+def get_headers():
+    headers = {}
+    if is_internal_auth():
+        access_token = get_access_token()
+        headers = {
+            "x-dassana-tenant-id": get_tenant_id(),
+            "Authorization": f"Bearer {access_token}", 
+        }
+    else:
+        app_token = get_dassana_token()
+        headers = {
+            "Authorization": f"Dassana {app_token}"
+        }
+    return headers
+
+def get_exc_str(exc):
+    return str(exc.replace("\"", "").replace("'", "").replace("\n"," ").replace("\t"," "))
+
 @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def get_ingestion_config(ingestion_config_id, app_id, tenant_id):
+    app_url = get_app_url()
     url = f"https://{app_url}/app/{app_id}/ingestionConfig/{ingestion_config_id}"
-    access_token = get_access_token()
-    headers = {
-        "x-dassana-tenant-id": tenant_id,
-        "Authorization": f"Bearer {access_token}", 
-    }
+    headers = get_headers()
     if app_url.endswith("svc.cluster.local:443"):
         response = requests.request("GET", url, headers=headers, verify=False)
     else:
@@ -90,12 +98,9 @@ def get_ingestion_config(ingestion_config_id, app_id, tenant_id):
 
 @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def patch_ingestion_config(payload, ingestion_config_id, app_id, tenant_id):
+    app_url = get_app_url()
     url = f"https://{app_url}/app/{app_id}/ingestionConfig/{ingestion_config_id}"
-    access_token = get_access_token()
-    headers = {
-        "x-dassana-tenant-id": tenant_id,
-        "Authorization": f"Bearer {access_token}",
-    }
+    headers = get_headers()
     if app_url.endswith("svc.cluster.local:443"):
         response = requests.request("PATCH", url, headers=headers, json=payload, verify=False)
     else:
@@ -105,14 +110,15 @@ def patch_ingestion_config(payload, ingestion_config_id, app_id, tenant_id):
 
 @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def get_access_token():
+    auth_url = get_auth_url()
     url = f"{auth_url}/oauth/token"
     if auth_url.endswith("svc.cluster.local"):
         response = requests.post(
             url,
             data={
                 "grant_type": "client_credentials",
-                "client_id": client_id,
-                "client_secret": client_secret,
+                "client_id": get_client_id(),
+                "client_secret": get_client_secret(),
             },
             verify=False
         )  
@@ -121,8 +127,8 @@ def get_access_token():
             url,
             data={
                 "grant_type": "client_credentials",
-                "client_id": client_id,
-                "client_secret": client_secret,
+                "client_id": get_client_id(),
+                "client_secret": get_client_secret(),
             }
         )
     try:
@@ -133,67 +139,11 @@ def get_access_token():
     return access_token
 
 @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
-def update_ingestion_to_done(job_id, tenant_id, metadata):
-    
-    access_token = get_access_token()
-    headers = {
-        "x-dassana-tenant-id": tenant_id,
-        "Authorization": f"Bearer {access_token}", 
-    }
-    res = requests.post(ingestion_service_url +"/job/"+job_id+"/"+"done", headers=headers, json={
-        "metadata": metadata
-    })
-    print("Ingestion status updated to done")
-    return res.json()
-
-@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
-def cancel_ingestion_job(job_id, tenant_id, metadata, fail_type):
-    
-    access_token = get_access_token()
-    headers = {
-        "x-dassana-tenant-id": tenant_id,
-        "Authorization": f"Bearer {access_token}", 
-    }
-    res = requests.post(ingestion_service_url +"/job/"+job_id+"/"+fail_type, headers=headers, json={
-        "metadata": metadata
-    })
-    print("Ingestion status updated to " + str(fail_type))
-    return res.json()
-
-@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
-def get_ingestion_details(tenant_id, source, record_type, config_id, metadata, priority, is_snapshot):
-    access_token = get_access_token()
-
-    headers = {
-        "x-dassana-tenant-id": tenant_id,
-        "Authorization": f"Bearer {access_token}", 
-    }
-    json_body = {
-        "source": str(source),
-        "recordType": str(record_type),
-        "configId": str(config_id),
-        "is_snapshot": is_snapshot,
-        "priority": priority,
-        "metadata": metadata
-        }
-    
-    if json_body["priority"] is None:
-        del json_body["priority"]
-    
-    res = requests.post(ingestion_service_url +"/job/", headers=headers, json=json_body)
-    if(res.status_code == 200):
-        return res.json()
-
-    return 0
-
-@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def report_status(status, additionalContext, timeTakenInSec, recordsIngested, ingestion_config_id, app_id, tenant_id):
+    app_url = get_app_url()
     reportingURL = f"https://{app_url}/app/v1/{app_id}/status"
 
-    headers = {
-        "x-dassana-tenant-id": tenant_id,
-        "Authorization": f"Bearer {get_access_token()}",
-    }
+    headers = get_headers()
 
     payload = {
         "status": status,
@@ -215,7 +165,7 @@ def report_status(status, additionalContext, timeTakenInSec, recordsIngested, in
 
 class DassanaWriter:
     def __init__(self, tenant_id, source, record_type, config_id, metadata = {}, priority = None, is_snapshot = False):
-        print("Initialized common utility")
+        logging.info("Initialized common utility")
 
         self.source = source
         self.record_type = record_type
@@ -225,6 +175,9 @@ class DassanaWriter:
         self.is_snapshot = is_snapshot
         self.tenant_id = tenant_id
         self.bytes_written = 0
+        self.fail_counter = 0
+        self.pass_counter = 0
+        self.debug_log = set()
         self.storage_service = None
         self.client = None
         self.aws_iam_role_arn = None
@@ -234,11 +187,13 @@ class DassanaWriter:
         self.bucket_name = None
         self.blob = None
         self.full_file_path = None
+        self.headers = get_headers()
+        self.ingestion_service_url = get_ingestion_srv_url()
+        self.is_internal_auth = is_internal_auth()
         self.file_path = self.get_file_path()
         self.job_id = None
-        self.initialize_client(self.tenant_id, self.source, self.record_type, self.config_id, self.metadata, self.priority, self.is_snapshot)
+        self.initialize_client()
         self.file = open(self.file_path, 'a')
-        
 
     def get_file_path(self):
         epoch_ts = int(time.time())
@@ -248,11 +203,11 @@ class DassanaWriter:
         with open(self.file_path, 'rb') as file_in:
             with gzip.open(f"{self.file_path}.gz", 'wb') as file_out:
                 file_out.writelines(file_in)
-        print("Compressed file completed")
+        logging.info("Compressed file completed")
     
-    def initialize_client(self, tenant_id, source, record_type, config_id,  metadata, priority, is_snapshot):
+    def initialize_client(self):
         try:
-            response = get_ingestion_details(tenant_id, source, record_type, config_id, metadata, priority, is_snapshot)
+            response = self.get_ingestion_details()
             
             self.storage_service = response['stageDetails']['cloud']
             self.job_id = response["jobId"]
@@ -271,7 +226,6 @@ class DassanaWriter:
             
             os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service_account.json'
             self.client = storage.Client()
-
         elif self.storage_service == 'aws':
             stage_details = response['stageDetails']
             if "awsIamRoleArn" in stage_details:
@@ -294,16 +248,16 @@ class DassanaWriter:
             self.upload_to_cloud()
             self.file_path = self.get_file_path()
             self.file = open(self.file_path, 'a')
-            print(f"Ingested data: {self.bytes_written} bytes")
+            logging.info(f"Ingested data: {self.bytes_written} bytes")
             self.bytes_written = 0
-        
-            
 
     def upload_to_cloud(self):
-        if self.client is None:
+        if self.client is None and self.is_internal_auth:
             raise ValueError("Client not initialized.")
 
-        if self.storage_service == 'gcp':
+        if not self.is_internal_auth:
+            self.upload_to_signed_url()
+        elif self.storage_service == 'gcp':
             self.upload_to_gcp()
         elif self.storage_service == 'aws':
             self.upload_to_aws()
@@ -336,11 +290,26 @@ class DassanaWriter:
         
         self.client.upload_file(self.file_path, self.bucket_name, self.file_path)
 
-    def cancel_job(self, error_code, failure_reason, debug_log, pass_counter = 0, fail_counter = 0, fail_type = "failed"):
+    def upload_to_signed_url(self):
+        signed_url = self.get_signing_url()
+        if not signed_url:
+            raise ValueError("The signed URL has not been received")
+        
+        headers = {
+            'Content-Encoding': 'gzip',
+            'Content-Type': 'application/octet-stream'
+        }
+        with open(str(self.file_path) + ".gz", "rb") as read:
+            data = read.read()
+            requests.put(url=signed_url, data=data, headers=headers)
+
+    def cancel_job(self, error_code, failure_reason, fail_type = "failed"):
         metadata = {}
-        job_result = {"failure_reason": failure_reason, "status": str(fail_type), "debug_log": debug_log, "pass": pass_counter, "fail": fail_counter, "error_code": error_code}
+        fail_type_status_metadata = "canceled" if str(fail_type) == "cancel" else str(fail_type)
+        self.debug_log.add(get_exc_str(str(failure_reason)))
+        job_result = {"failure_reason": failure_reason, "status": fail_type_status_metadata, "debug_log": list(self.debug_log), "pass": self.pass_counter, "fail": self.fail_counter, "error_code": error_code}
         metadata["job_result"] = job_result
-        cancel_ingestion_job(self.job_id, self.tenant_id, metadata, fail_type)
+        self.cancel_ingestion_job(metadata, fail_type)
         if os.path.exists("service_account.json"):
             os.remove("service_account.json")
 
@@ -348,57 +317,106 @@ class DassanaWriter:
         if os.path.exists("service_account.json"):
             os.remove("service_account.json")
         try:
+            str_exc = get_exc_str(str(exception_from_src))
             if(type(exception_from_src).__name__ == "ExternalError"):
                 metadata = {}
-                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": [str(exception_from_src)], "error_code": "other_error"}
+                self.debug_log.add(str_exc)
+                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": list(self.debug_log), "pass": self.pass_counter, "fail": self.fail_counter, "error_code": "other_error"}
                 metadata["job_result"] = job_result
-                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "failed")
+                self.cancel_ingestion_job(metadata, "failed")
 
             elif(type(exception_from_src).__name__ == "AuthenticationError"):
-                
-                debug_log = ["Auth Response: " + str(exception_from_src.response) + " Stack Trace: " + str(exception_from_src)]
+                self.debug_log.add("Auth Response: " + get_exc_str(str(exception_from_src.response)) + " Stack Trace: " + str_exc)
                 metadata = {}
-                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": debug_log, "error_code": "auth_error"}
+                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": list(self.debug_log), "pass": self.pass_counter, "fail": self.fail_counter, "error_code": "auth_error"}
                 metadata["job_result"] = job_result
-                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "failed")
+                self.cancel_ingestion_job(metadata, "failed")
 
             elif(type(exception_from_src).__name__ == "InternalError"):
+                self.debug_log.add(str_exc)
                 metadata = {}
-                job_result = {"failure_reason": exception_from_src.message, "status": "cancel", "debug_log": [str(exception_from_src)], "error_code": "other_error"}
+                job_result = {"failure_reason": exception_from_src.message, "status": "canceled", "debug_log": list(self.debug_log), "pass": self.pass_counter, "fail": self.fail_counter, "error_code": "other_error"}
                 metadata["job_result"] = job_result
-                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "cancel")
+                self.cancel_ingestion_job(metadata, "cancel")
 
             elif(type(exception_from_src).__name__ == "StageWriteFailure"):
                 metadata = {}
-                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": [str(exception_from_src)], "error_code": "stage_write_failure"}
+                self.debug_log.add(str_exc)
+                job_result = {"failure_reason": exception_from_src.message, "status": "failed", "debug_log": list(self.debug_log), "pass": self.pass_counter, "fail": self.fail_counter, "error_code": "stage_write_failure"}
                 metadata["job_result"] = job_result
-                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "failed")
+                self.cancel_ingestion_job(metadata, "failed")
             
             else:
                 metadata = {}
-                job_result = {"failure_reason": str(exception_from_src), "status": "cancel", "debug_log": [str(exception_from_src)], "error_code": "other_error"}
+                self.debug_log.add(str_exc)
+                job_result = {"failure_reason": str(exception_from_src), "status": "canceled", "debug_log": list(self.debug_log), "pass": self.pass_counter, "fail": self.fail_counter, "error_code": "other_error"}
                 metadata["job_result"] = job_result
-                cancel_ingestion_job(self.job_id, self.tenant_id, metadata, "cancel")
+                self.cancel_ingestion_job(metadata, "cancel")
         
         except Exception as e:
             metadata = {}
-            job_result = {"failure_reason": str(e), "status": "cancel", "debug_log": [str(e)], "error_code": "other_error"}
+            job_result = {"failure_reason": str(e), "status": "canceled", "debug_log": [str(e)], "pass": self.pass_counter, "fail": self.fail_counter, "error_code": "other_error"}
             try:
-                cancel_ingestion_job(self.job_id, self.tenant_id, {}, "cancel")
+                self.cancel_ingestion_job({}, "cancel")
             except:
                 raise
             
-    def close(self, pass_counter, fail_counter, debug_log = set()):
+    def close(self):
         self.file.close()
         metadata = {}
-        job_result = {"status": "ready_for_download", "source": {"pass" : int(pass_counter), "fail": int(fail_counter), "debug_log": list(debug_log)}}
+        job_result = {"status": "ready_for_loading", "source": {"pass" : int(self.pass_counter), "fail": int(self.fail_counter), "debug_log": list(self.debug_log)}}
         metadata["job_result"] = job_result
         if self.bytes_written > 0:
             self.compress_file()
             self.upload_to_cloud()
-            print(f"Ingested remaining data: {self.bytes_written} bytes")
+            logging.info(f"Ingested remaining data: {self.bytes_written} bytes")
             self.bytes_written = 0
-        update_ingestion_to_done(self.job_id, self.tenant_id, metadata)
+        self.update_ingestion_to_done(metadata)
         if os.path.exists("service_account.json"):
             os.remove("service_account.json")
 
+    @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
+    def update_ingestion_to_done(self, metadata):
+        
+        res = requests.post(self.ingestion_service_url +"/job/"+self.job_id+"/"+"done", headers=self.headers, json={
+            "metadata": metadata
+        })
+        logging.info("Ingestion status updated to done")
+        return res.json()
+
+    @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
+    def get_ingestion_details(self):
+        
+        json_body = {
+            "source": str(self.source),
+            "recordType": str(self.record_type),
+            "configId": str(self.config_id),
+            "is_snapshot": self.is_snapshot,
+            "priority": self.priority,
+            "metadata": self.metadata
+            }
+        
+        if json_body["priority"] is None:
+            del json_body["priority"]
+        
+        res = requests.post(self.ingestion_service_url +"/job/", headers=self.headers, json=json_body)
+        if(res.status_code == 200):
+            logging.info("Ingestion job created")
+            return res.json()
+
+        return 0
+
+    @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
+    def cancel_ingestion_job(self, metadata, fail_type):
+        
+        res = requests.post(self.ingestion_service_url +"/job/"+ self.job_id +"/"+fail_type, headers=self.headers, json={
+            "metadata": metadata
+        })
+        logging.info("Ingestion status updated to " + str(fail_type))
+        return res.json()
+
+    @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
+    def get_signing_url(self):
+        res = requests.get(self.ingestion_service_url +"/job/"+self.job_id+"/"+"signing-url", headers=self.headers)
+        signed_url = res["url"]
+        return signed_url
