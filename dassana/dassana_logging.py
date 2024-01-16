d1989b55cf62bd89284d04ee9d8c53c666ca3db5
Karthik Reddy <111749101+karthik-dassana@users.noreply.github.com>
2024-01-16 16:24:52 +0530

    Update dassana_logging.py

--- dassana/dassana_logging.py
+++ dassana/dassana_logging.py
@@ -1,10 +1,7 @@
 import datetime
-import json
 from uuid import uuid4
-
-from .common import *
+from dassana import common
 from .dassana_env import *
-
 from typing import Final
 import logging
 from google.cloud import pubsub_v1
@@ -23,7 +20,7 @@ publisher = pubsub_v1.PublisherClient()
 event_topic_name = None
 
 if dassana_partner:
-    event_topic_name = dassana_partner + "_LOG_EVENT_TOPIC_NAME"
+    # event_topic_name = dassana_partner + "_LOG_EVENT_TOPIC_NAME"
 
 scope_id_mapping = {
     "crowdstrike_edr": "detection",
@@ -42,15 +39,15 @@ scope_id_mapping = {
     "ms_defender_endpoint_vulnerability": "vulnerability"
 }
 
-def log(source, status=None, exception=None, locals={}, scope_id=None, config_id=None, metadata={}):
-    states = build_state(source, scope_id, config_id, locals, status, exception=exception)
+def log(source, status=None, exception=None, locals={}, scope_id=None, config_id=None, metadata={}, job_id=None):
+    states = build_state(source, scope_id, config_id, locals, job_id, status, exception=exception)
     
     for state in states:
         message = {}
 
         message["developerCtx"] = {}
         message["developerCtx"].update(state)
-        message["developerCtx"].update(add_developer_context(metadata, exception))
+        message["developerCtx"].update(add_developer_context(metadata, status, exception))
 
         message["customerCtx"] = {}
         message["customerCtx"].update(state)
@@ -66,30 +63,36 @@ def log(source, status=None, exception=None, locals={}, scope_id=None, config_id
         if dassana_partner:
             common.publish_message(message, project_id, event_topic_name)
 
-def add_developer_context(metadata, exception):
+def add_developer_context(metadata, status ,exception):
     state = {}
 
     if metadata:
         state["pass"] = metadata["source"]["pass"]
         state["fail"] = metadata["source"]["fail"]
         state["debugLog"] = metadata["source"]["debug_log"]
-
-    if exception:
+    
+    if status == 'failed':
+        if exception:
+            state["errorDetails"] = {}
+            if isinstance(exception, exc.DassanaException):
+                state["errorDetails"]["errorCode"] = exception.error_type
+                state["errorDetails"]["isInternal"] = exception.is_internal
+                state["errorDetails"]["isAutoRecoverable"] = exception.is_auto_recoverable
+                if exception.error_type == "internal_error":
+                    if exception.error_details:
+                        state["errorDetails"]["errorMessage"] = exception.error_details
+                    else:
+                        state["errorDetails"]["errorMessage"] = exception.message
+                if isinstance(exception, exc.ApiError):
+                    state["errorDetails"]["httpRequest"] = exception.http_request.__dict__
+                    state["errorDetails"]["httpResonse"] = exception.http_response.__dict__ 
+                return state
+            
         state["errorDetails"] = {}
-        if isinstance(exception, exc.DassanaException):
-            state["errorDetails"]["errorCode"] = exception.error_type
-            state["errorDetails"]["isInternal"] = exception.is_internal
-            state["errorDetails"]["isAutoRecoverable"] = exception.is_auto_recoverable
-            if exception.error_type == "internal_error":
-                state["errorDetails"]["errorMessage"] = exception.message
-            if isinstance(exception, exc.ApiError):
-                state["errorDetails"]["httpRequest"] = exception.http_request.__dict__
-                state["errorDetails"]["httpResonse"] = exception.http_response.__dict__
-        else:
-            state["errorDetails"]["errorCode"] = "internal_error"
-            state["errorDetails"]["errorMessage"] = "Unexpected error occurred while collecting data"
-            state["errorDetails"]["isInternal"] = True
-            state["errorDetails"]["isAutoRecoverable"] = False
+        state["errorDetails"]["errorCode"] = "internal_error"
+        state["errorDetails"]["errorMessage"] = str(exception)
+        state["errorDetails"]["isInternal"] = True
+        state["errorDetails"]["isAutoRecoverable"] = False
     return state
   
 def add_customer_context(state, exception=None):
@@ -97,56 +100,53 @@ def add_customer_context(state, exception=None):
     state["status"] = "successful" if state.get("status") == "ready_for_loading" else state.get("status")
     state["tenantId"] = dassana_partner_tenant_id
     state["siteId"] = dassana_partner_client_id
-    
-    if exception:
-        state["errorDetails"] = {}
-        if isinstance(exception, exc.DassanaException):
-            if not exception.is_internal:
-                if isinstance(exception, exc.ApiError):
-                    state["errorDetails"]["message"] = exception.message
-                    state["errorDetails"]["httpRequest"] = exception.http_request.__dict__
-                    state["errorDetails"]["httpResponse"] = exception.http_response.__dict__
-                    return state
-        state["errorDetails"]["message"] = "Job terminated due to inactivity"
+
+    if state["status"] == 'failed':
+        if exception:
+            state["errorDetails"] = {}
+            if isinstance(exception, exc.DassanaException):
+                if not exception.is_internal:
+                    if isinstance(exception, exc.ApiError):
+                        state["errorDetails"]["message"] = exception.message
+                        state["errorDetails"]["httpRequest"] = exception.http_request.__dict__
+                        state["errorDetails"]["httpResponse"] = exception.http_response.__dict__
+                        return state
+        state["errorDetails"]["message"] = "Job terminated due to internal error"
     return state
 
-def build_state(source, scope_id, config_id, locals, status=None, exception=None):
+def build_state(source, scope_id, config_id, locals, job_id, status=None, exception=None):
     state = {}
 
     state["eventId"] = str(uuid4())
-    state["timestamp"] = datetime.datetime.utcnow().isoformat()
-    state["source"] = source
-    state["status"] = status
+    state["timestamp"] = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%3fZ")
+    state["connector"] = source
+    if not status:
+        state["status"] = "in_progress"
+    else:
+        state["status"] = status
+    state["level"] = "info" if status in ['ready_for_loading', 'in_progress'] else "error"
+
+    if job_id:
+        state["jobId"] = job_id
 
     if status == "failed":
-        if locals.get("selected_scope_ids") and len(locals.get("selected_scope_ids")) > 0:
-            scope_id_str = ','.join(locals.get("selected_scope_ids"))
-            state["message"] = f"failed to run data collection for {scope_id_str}"
+        if not locals.get("config_id") or locals.get("selected_scope_ids"):
+            state["message"] = "failed to finish data collection for all the scopes"
         else:
             state["message"] = "failed to finish data collection"
 
     elif status == "in_progress":
         state["message"] = "starting data collection"
+
     else:
         state["message"] = "successfully finished data collection"
 
     if config_id:
-        state["configId"] = config_id
+        state["connectionId"] = config_id
     else:
-        if locals.get("config_id"):
-            state["configId"] = locals.get("config_id")
+        state["connectionId"] = locals.get("config_id")
       
     if scope_id:
         state["scopeId"] = scope_id_mapping.get(scope_id, scope_id)
-    else:
-        results = []
-        scope_ids = locals.get("selected_scope_ids")
-        if isinstance(scope_ids, list):
-            for scope_id in scope_ids:
-                if exception and locals.get(f'{scope_id}_writer'):
-                    continue
-                state["scopeId"] = scope_id_mapping.get(scope_id, scope_id)
-                results.append(state)
-            return results
 
     return [state]
